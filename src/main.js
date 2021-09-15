300a9af3f30faa544c2827c8b5978ed24f90b426
xiaohongZHAN <2459522339@qq.com>
2021-09-15 11:36:55 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -4,26 +4,40 @@ const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
 const hashMap = xObject || [{
         logo: 'A',
-        logoType: 'text',
         url: 'https://www.acfun.cn',
     },
     {
-        logo: './images/bilibili.jpg',
-        logoType: 'image',
+        logo: 'B',
         url: 'https://www.bilibili.com',
     },
 ]
+const simplifyUrl = (url) => {
+    return url.replace('http://', '')
+        .replace('https://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '') //删除以/开头的东西
+}
 const render = () => {
     $siteList.find('li:not(.Last)').remove()
-    hashMap.forEach(node => {
+    hashMap.forEach((node, index) => {
         const $li = $(`<li>
-        <a href="${node.url}">
         <div class="site">
             <div class="logo">${node.logo[0]}</div>
-            <div class="link">${ node.url }</div>
+            <div class="link">${ simplifyUrl(node.url )}</div>
+            <div class="close">
+            <svg class="icon" >
+        <use xlink:href="#icon-searchclose"></use>
+           </svg></div>
         </div>
-    </a>
         </li>`).insertBefore($lastLi)
+        $li.on('click', () => {
+            window.open(node.url)
+        })
+        $li.on('click', '.close', (e) => {
+            e.stopPropagation() //阻止冒泡，防止点击close也点击li然后跳转页面
+            hashMap.splice(index, 1)
+            render() //删除后重新渲染
+        })
     })
 }
 
@@ -38,7 +52,7 @@ $('.addButton')
         }
         console.log(url)
         hashMap.push({
-            logo: url[0],
+            logo: simplifyUrl(url)[0].toUpperCase(),
             logoType: 'text',
             url: url,
         })
@@ -52,3 +66,11 @@ window.onbeforeunload = () => {
 
     localStorage.setItem('x', string)
 }
+$(document).on('keypress', (e) => {
+    const key = e.key
+    for (let i = 0; i < hashMap.length; i++) {
+        if (hashMap[i].logo.toLowerCase() === key) {
+            window.open(hashMap[i].url)
+        }
+    }
+})
