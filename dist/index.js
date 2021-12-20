6fcc440bdc27542fbbe0db00c8b82ae534652cf0
xiaohongZHAN <2459522339@qq.com>
2021-12-20 15:00:31 +0800

    update input onfocus

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51767" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60995" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
